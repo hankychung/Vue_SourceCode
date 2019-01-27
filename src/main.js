5ae81bff645aa3c79cb770a5037edafa7c574c73
Han <hanky_chung@hotmail.com>
2019-01-27 22:09:16 +0800

    add Compiler & Dep & Watcher

--- src/main.js
+++ src/main.js
@@ -1,21 +1,141 @@
+class Compiler {
+  constructor(el, vm) {
+    this.$el = el
+    this.$data = vm
+    if (this.$el) {
+      // 遍历所有节点，将节点存入内存碎片
+      this.$fragment = this.toFragment(this.$el)
+      // 编译
+      this.compile(this.$fragment)
+      // 输出到节点
+      this.$el.appendChild(this.$fragment)
+    }
+  }
+
+  toFragment(el) {
+    console.log(el)
+    let fragment = document.createDocumentFragment()
+    while(el.firstChild) {
+      fragment.appendChild(el.firstChild)
+    }    
+    console.log(fragment)
+    return fragment
+  }
+
+  compile(frag) {
+    console.log(frag.childNodes)
+    let nodes = Array.from(frag.childNodes)
+    console.log(nodes)
+    nodes.forEach(node => {
+      console.dir(node)
+      if (this.isElement(node)) {
+
+      }
+      if (this.isText(node)) {
+        this.handleText(node)
+      }
+    })
+  }
+  // 节点是否为元素
+  isElement(node) {
+    if (node.nodeType === 1) return true
+  }
+  // 节点是否为文本
+  isText(node) {
+    if (node.nodeType === 3) return true
+  }
+
+  // 判断是否含有vue变量 - {{}}
+  handleText(node) {    
+    let ctn = node.textContent
+    let reg = /\{\{(.*)\}\}/
+    if (reg.test(ctn)) {
+      console.log(ctn)
+      console.log(RegExp.$1)
+      let exp = RegExp.$1
+      this.updator(node, exp)
+    }
+  }
+
+  updator(node, exp) {
+    console.log('updator', node, exp)
+    console.dir(node)        
+    new Watcher(this.$data, exp, () => {
+      console.log('inUpdator', node)
+      console.dir(node)
+      node.textContent = this.$data[exp]
+      return this.$data[exp]
+    })
+  }
+}
+
+class Watcher {
+  constructor(vm, key, cb) {
+    this.vm = vm
+    this.key = key
+    this.cb = cb
+    this.val = this.get()
+  }
+  get() {
+    Dep.target = this.cb
+    let val = this.cb()
+    return val
+  }
+}
+
+class Dep {
+  constructor() {
+    this.deps = []
+  }
+  collect(cb) {
+    this.deps.push(cb)
+  }
+  update() {
+    this.deps.forEach(dep => {
+      dep()
+    })
+  }
+}
+
+
 class H_Vue {
   constructor(vm) {
     this.$el = vm.el 
     this.$data = vm.data
     this.observer(this.$data)
+    new Compiler(this.$el, this.$data)    
   }
   observer(datas) {
     Object.keys(datas).forEach(key => {
-      this.reactive(datas, key, datas[key])
+      this.proxyData(key)
+      this.defineReactive(datas, key, datas[key])
     })
   }
-  reactive(obj, key, val) {
+  // 数据代理
+  proxyData(key) {
+    Object.defineProperty(this, key, {
+      get() {
+        return this.$data[key]
+      },
+      set(newVal) {
+        this.$data[key] = newVal
+      }
+    })
+  }
+  // 数据响应
+  defineReactive(obj, key, val) {
+    let dep = new Dep()
     Object.defineProperty(obj, key, {
       get() {
+        if (dep.deps.indexOf(Dep.target) == -1) {
+          dep.collect(Dep.target)
+        }        
         return val
       },
       set(newVal) {
         if (newVal == val) return
+        val = newVal
+        dep.update()
         console.log('change!!!!', newVal)
       }
     })
